b4e0f74097f2bfcc9cf8530ab326268fbce89913
MHWillard <mhwillard@gmail.com>
2021-07-26 06:38:44 -0400

    adding footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return(
+        <div>
+            <div>
+            Like what you see? Join fi.do and connect with high-quality dog freelancers and clients.
+            Learn more, or sign up.
+            </div>
+            <div>
+            Privacy Policy / Sitemap / Contact Us
+            Copyright 2021 fi.do.
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
--- client/src/components/MainContainer.tsx
+++ client/src/components/MainContainer.tsx
@@ -1,9 +1,13 @@
 import ContainerContent from '../components/ContainerContent';
+import NavbarButtons from '../components/NavbarButtons'
+import Footer from '../components/Footer'
 
 function MainContainer() {
     return(
         <div className="main-container">
+            <NavbarButtons />
             <ContainerContent />
+            <Footer />
         </div>
     );
 }
